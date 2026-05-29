d0068437ae892c64adbe224e673213ae0ea03be5
Sri Padamati <sri.padamati@brillio.com>
2026-05-29 20:31:13 +0530

    protected route

--- library-frontend/src/App.js
+++ library-frontend/src/App.js
@@ -1,13 +1,14 @@
 import Login from "./components/Login";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./Dashboard";
+import ProtectedRoute from "./ProtectedRoute";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login/>}></Route>
-        <Route path="/dashboard" element={<Dashboard/>}></Route>
+        <Route path="/dashboard" element = {<ProtectedRoute><Dashboard/></ProtectedRoute>}></Route>
       </Routes>
     </BrowserRouter>
   );
