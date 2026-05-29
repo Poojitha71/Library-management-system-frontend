21063e0403a8518d504f2fc2b62c8ab5c40e4953
Sri Padamati <sri.padamati@brillio.com>
2026-05-29 19:43:04 +0530

    hooks practice

--- library-frontend/src/App.js
+++ library-frontend/src/App.js
@@ -1,11 +1,15 @@
 import Login from "./components/Login";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./Dashboard";
 
 function App() {
-
   return (
-    <div>
-      <Login />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login/>}></Route>
+        <Route path="/dashboard" element={<Dashboard/>}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- library-frontend/src/components/Login.jsx
+++ library-frontend/src/components/Login.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function Login() {
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
   const handleLogin = async () => {
 
@@ -23,6 +25,7 @@ function Login() {
       localStorage.setItem("token", response.data.token);
 
       alert("Login Successful");
+      navigate("/dashboard");
 
     } catch (error) {
 
