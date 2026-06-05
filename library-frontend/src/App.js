bc3f5e28d33eb75261bd897ed36d651fcb38905f
Sri Padamati <sri.padamati@brillio.com>
2026-06-05 17:22:03 +0530

    dashboard code

--- library-frontend/src/App.js
+++ library-frontend/src/App.js
@@ -54,6 +54,8 @@ function App() {
           </ProtectedRoute>
         }
       />
+
+      <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
   );
 }
--- library-frontend/src/Pages/Dashboard.jsx
+++ library-frontend/src/Pages/Dashboard.jsx
@@ -1,10 +1,75 @@
-import React from 'react'
-import Books from './Books'
-export default function Dashboard() {
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Navbar from "../components/Navbar";
+import "../styles/Dashboard.css";
+
+function Dashboard() {
+  const [stats, setStats] = useState({
+    totalBooks: 0,
+    availableBooks: 0,
+    borrowedBooks: 0,
+    totalUsers: 0,
+  });
+  const role = localStorage.getItem("role");
+
+  useEffect(() => {
+    fetchDashboardStats();
+  }, []);
+
+  const fetchDashboardStats = async () => {
+    try {
+      const token = localStorage.getItem("token");
+
+      const response = await axios.get(
+        "http://localhost:8080/dashboard/stats",
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      setStats(response.data);
+    } catch (error) {
+      console.log(error);
+
+      alert("Failed to load dashboard");
+    }
+  };
+
   return (
-    <div>
-      <h1>Dashboard page</h1>
-      <Books/>
-    </div>
-  )
+    <>
+      <Navbar />
+
+      <div className="dashboard-container">
+        <h1>📚 Library Dashboard</h1>
+
+        <div className="dashboard-grid">
+          <div className="dashboard-card">
+            <h3>Total Books</h3>
+            <p>{stats.totalBooks}</p>
+          </div>
+
+          <div className="dashboard-card">
+            <h3>Available Books</h3>
+            <p>{stats.availableBooks}</p>
+          </div>
+
+          <div className="dashboard-card">
+            <h3>Borrowed Books</h3>
+            <p>{stats.borrowedBooks}</p>
+          </div>
+
+          {role === "ADMIN" && (
+            <div className="dashboard-card">
+              <h3>Total Users</h3>
+              <p>{stats.totalUsers}</p>
+            </div>
+          )}
+        </div>
+      </div>
+    </>
+  );
 }
+
+export default Dashboard;
